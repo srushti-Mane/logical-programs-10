7876b0d1d7ba5880fba77dc7e91d5db21f9d33f4
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-05 18:32:58 +0530

    modified:logic for factor of number

---
+++ src/FactorsOfNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FactorsOfNumber {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int num=sc.nextInt();
+            System.out.print("Factors of " + num + " are: ");
+            for (int i = 1; i <= num; ++i) {
+                if (num % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+
